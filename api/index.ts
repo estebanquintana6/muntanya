88ffe8463cc7fac06492b8bbb3c2b6e1701afaf4
Esteban Quintana <estebanquintana@Estebans-MacBook-Pro.local>
2024-08-11 14:56:06 -0600

    Connecting to mongo

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,38 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config()
+
+const { MONGO_URI } = process.env;
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(cors());
+app.options('*', cors());
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
+
+const mongoConnection = () => mongoose.connect(MONGO_URI || "");
+
+mongoConnection();
+
+const db = mongoose.connection;
+
+db.on("error", (err: Error) => {
+    console.log("There was a problem connecting to mongo: ", err);
+    console.log("Trying again");
+    setTimeout(() => mongoConnection(), 5000);
+});
+db.once("open", async () => {
+    await mongoConnection();
+    console.log("Successfully connected to mongo");
+});
+
+app.get("/", (req, res) => res.send("Muntanya server up & running"));
+
+app.listen(4000, () => console.log("Server ready on port 4000."));
 
 export default app;
